95ed5be5b1ada4d080ac2de8c8dc3e90601301da
Josh <110058923+Helios116@users.noreply.github.com>
2022-09-07 17:12:07 -0600

    Create dice_roll.js

---
+++ dice_roll.js
@@ -0,0 +1,26 @@
+
+//Dice selection
+var dice = parseInt(prompt("What sided die to you want to roll?"));
+
+
+//Equa-partition bins to place random number into using finite precision limit (simulates the "fair dice" aspect)
+x = .999999999999999999/dice;
+
+
+//seeding the random number (between 0 and 0.999...)
+var n = Math.random();
+
+
+//Dice rolling engine that allows for nothing less than 1 and nothing more than the highest value of the dice selected
+if (n < x) {
+    n = Math.floor((n*dice)+1)
+}
+    else if (n < x*(dice-1)) {
+        n = Math.floor((n*dice))
+    }
+    else if (n > x*(dice-1)){
+        n = Math.ceil((n*dice))
+    }
+
+//Display the dice roll result
+alert(n);
